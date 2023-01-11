35761ae9f18ea2bf7f7381a981d1d9c4c657003e
jangheon Pyo <ifheone@gmail.com>
2023-01-11 16:30:37 +0900

    모스부호(1)

---
+++ programmers/v0/MorseCode(모스부호(1))/answer.js
@@ -0,0 +1,38 @@
+function solution(letter) {
+  let morse = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+  };
+  let str = letter.split(" ");
+  const arr = [];
+  str.forEach((ele, i) => {
+    if (Object.keys(morse).includes(ele)) {
+      return arr.push(morse[ele]);
+    }
+  });
+  return arr.join("");
+}
