38bd2e6511862439fd198387acec4d5520f8a0bf
jangheon Pyo <ifheone@gmail.com>
2023-07-11 22:45:58 +0900

    조건에 맞게 수열 변환하기 3

---
+++ programmers/v0/ConvertingSequencesAccordingToConditions3(조건에맞게수열변환하기3)/answer.js
@@ -0,0 +1,13 @@
+function solution(arr, k) {
+  var answer = [];
+  if (k % 2 === 0) {
+    answer = arr.map((a) => {
+      return a + k;
+    });
+  } else {
+    answer = arr.map((a) => {
+      return a * k;
+    });
+  }
+  return answer;
+}
